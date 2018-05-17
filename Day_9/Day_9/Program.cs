b36bd5797e128d4e3b8abb36ec9a46c561560fe3
KernoKristaps <39062321+KernoKristaps@users.noreply.github.com>
2018-05-17 20:53:43 +0300

    1_summit

---
+++ Day_9/Day_9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tests TestaObjekts = new Tests();
+            int rezultats = TestaObjekts.Saskaitisana(3, 7);
+
+            Tests TestaObjekts2 = new Tests();
+
+            TestaObjekts.skaitlis = 3;
+            TestaObjekts2.skaitlis = 6;
+            Console.WriteLine("Izvade no TestaObjekts: " + TestaObjekts.skaitlis);
+            Console.WriteLine("Izvade no TestaObjekts2: " + TestaObjekts2.skaitlis);
+
+            Console.WriteLine("Izvade no klases: " + rezultats);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day_9/Day_9/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+    class Tests
+    {
+        public int skaitlis;
+
+        public int Saskaitisana(int a, int b)
+        {
+            Izvade();
+            return a + b;
+        }
+
+        private void Izvade()
+        {
+            Console.WriteLine("tests!");
+        }
+    }
+}
